70e9eeae58915fe455c9a3a7986362090ce887d3
Ali-Taabesh Sayyed <alitsayyed@gmail.com>
2025-05-16 18:31:04 -0500

    Updates to contact section and small other changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 'use client';
 
 import HeroSection from './components/HeroSection';
-import SystemArchitecture from './components/SystemArchitecture';
-import TechnicalMetrics from './components/TechnicalMetrics';
+import WorkExperience from './components/WorkExperience';
+import PersonalProjects from './components/PersonalProjects';
 import ContactSection from './components/ContactSection';
 
 export default function BackendPortfolio() {
-	return (
-		<main className="min-h-screen bg-[#1A1A1A] text-white overflow-x-hidden">
-			<HeroSection />
-			<SystemArchitecture />
-			<TechnicalMetrics />
-			<ContactSection />
-		</main>
-	);
+  return (
+    <main className="min-h-screen bg-[#1A1A1A] text-white overflow-x-hidden">
+      <HeroSection />
+      <WorkExperience />
+      <PersonalProjects />
+      <ContactSection />
+    </main>
+  );
 }
